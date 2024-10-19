d01cd353075946bc0456dca6128402fca5cf17e2
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-10-19 23:15:40 +0300

    add the posts and author models to the applications

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
+from users.models import Author
+import uuid
 
-# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        Author, 
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+    )
+    body = models.TextField()
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self) -> str:
+        return self.title[:50] + ("..." if len(self.title) > 50 else "")
