3a08d43d7c479cab7583a97bef352a764766dd62
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-10-22 08:10:21 +0300

    add the detail and list view of a post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+
+    context = {'posts': posts}
+
+    return render(
+        request, 
+        'blog/post/list.html',
+        context
+    )
+
+
+def post_details(request, uuid):
+    post = get_object_or_404(Post, uuid=uuid, status=Post.Status.PUBLISHED)
+    
+    context = {'post': post}
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        context
+    )
